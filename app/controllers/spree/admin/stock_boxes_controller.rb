ef0fd152e57e48ffa60175b89852936947c7ba90
otamm <oivatom@gmail.com>
2016-02-01 16:53:13 -0200

    Testing JSON requests on index

--- app/controllers/spree/admin/stock_boxes_controller.rb
+++ app/controllers/spree/admin/stock_boxes_controller.rb
@@ -40,7 +40,7 @@ module Spree
         
         respond_to do |format|
           format.html # show.html.erb
-          format.json { render json: @stock_box }
+          format.json { render json: @stock_box, @variants }
         end
       end
 
